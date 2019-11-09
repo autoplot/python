9bb53d5de23e265ae5adc5ae0a7d10db4a88896a
jbfaden <faden@cottagesystems.com>
2019-11-09 08:56:25 -0600

    add toDateTime

--- autoplot/__init__.py
+++ autoplot/__init__.py
@@ -1,4 +1,5 @@
 # Allow "from autoplot import javaaddpath"
 from autoplot.jpypeutil import javaaddpath
 from autoplot.jpypeutil import applot,ndarray2qdataset
+from autoplot.jpypeutil import toDateTime
 
