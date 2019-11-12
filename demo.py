80c4373f3cb5bbe42013b02a96031df55baeb8f7
Jeremy Faden <faden@cottagesystems.com>
2019-11-12 10:06:50 -0600

    switch to cdaweb

--- demo.py
+++ demo.py
@@ -3,13 +3,13 @@ from autoplot import javaaddpath,toDateTime
 org = javaaddpath('http://autoplot.org/jnlp/latest/autoplot.jar')
 apds = org.autoplot.idlsupport.APDataSet()
 
-apds.setDataSetURI('http://autoplot.org/data/swe-np.xls?column=data&depend0=dep0')
+apds.setDataSetURI('vap+cdaweb:ds=OMNI2_H0_MRG1HR&id=DST1800&timerange=Oct+2016')
 apds.doGetDataSet()
 
 print( apds.toString() )
 
-vv= apds.values()
-tt= toDateTime( apds, 'dep0' )
+vv= apds.values('DST' )
+tt= toDateTime( apds, 'Epoch' )
 
 from matplotlib import pyplot as plt
 
