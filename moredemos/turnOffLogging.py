6de00e2fc2a25d166bff0b9141e8c22ef4c378a9
Jeremy Faden <faden@cottagesystems.com>
2019-11-14 15:12:23 -0600

    Update turnOffLogging.py

--- moredemos/turnOffLogging.py
+++ moredemos/turnOffLogging.py
@@ -1,12 +1,17 @@
 # this shows how to turn off logging messages when apds is created.
 
+import autoplot
 import jpype
-logging= jpype.JPackage('java.util.logging')
 
+jpype.startJVM(jpype.getDefaultJVMPath(),'-Dclasspath=/tmp/autoplot.jar')
+
+# turn off logging
+logging= jpype.JPackage('java.util.logging')
 l= logging.Logger.getLogger('qdataset.bridge')
 l.setLevel(logging.Level.WARNING)
 
 org= javaaddpath('http://autoplot.org/devel/autoplot.jar')
+org= jpype.JPackage('org')
 
 apds = org.autoplot.idlsupport.APDataSet()
 apds.setDataSetURI('vap+cdaweb:ds=OMNI2_H0_MRG1HR&id=DST1800&timerange=Oct+2016')
