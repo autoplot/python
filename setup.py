6d9fd589196f8c0bfb239103ee6bae225ca142db
jbfaden <faden@cottagesystems.com>
2019-11-14 11:01:04 -0600

    update the version number

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if sys.argv[1] == 'develop':
 
 setup(
     name='autoplot',
-    version='0.1.2',
+    version='0.1.3',
     author='Jeremy Faden',
     author_email='faden@cottagesystems.com',
     packages=find_packages(), 
