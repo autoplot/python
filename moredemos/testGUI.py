c7c494bc31f6bffd5cba41cff523af8906ebf406
Jeremy Faden <faden@cottagesystems.com>
2019-11-18 11:31:07 -0600

    Update testGUI.py

--- moredemos/testGUI.py
+++ moredemos/testGUI.py
@@ -7,4 +7,4 @@ org = addjavapath('http://autoplot.org/latest/autoplot.jar')
 javax= jpype.JPackage('javax')
 javax.swing.JOptionPane.showMessageDialog(None, 'Java Swing is Okay')
 
-print('Things didn't hang, so this platform is okay.')
+print('Things didn''t hang, so this platform is okay.')
