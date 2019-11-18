118a931ce29b9e0d2dd8984ababf948c76d39a32
jbfaden <faden@cottagesystems.com>
2019-11-18 10:52:32 -0600

    pep8

--- moredemos/turnOffLogging.py
+++ moredemos/turnOffLogging.py
@@ -3,16 +3,18 @@
 import autoplot
 import jpype
 
-# wget -O /tmp/autoplot.jar https://ci-pw.physics.uiowa.edu/job/autoplot-release/1824/artifact/autoplot/Autoplot/dist/autoplot.jar
-jpype.startJVM(jpype.getDefaultJVMPath(),'-Djava.class.path=/tmp/autoplot.jar')
+# wget -O /tmp/autoplot.jar http://autoplot.org/latest/autoplot.jar
+jpype.startJVM(jpype.getDefaultJVMPath(),
+               '-Djava.class.path=/tmp/autoplot.jar')
 
 # turn off logging
-logging= jpype.JPackage('java.util.logging')
-l= logging.Logger.getLogger('qdataset.bridge')
+logging = jpype.JPackage('java.util.logging')
+l = logging.Logger.getLogger('qdataset.bridge')
 l.setLevel(logging.Level.WARNING)
 
-org= jpype.JPackage('org')
+org = jpype.JPackage('org')
 
 apds = org.autoplot.idlsupport.APDataSet()
-apds.setDataSetURI('vap+cdaweb:ds=OMNI2_H0_MRG1HR&id=DST1800&timerange=Oct+2016')
+apds.setDataSetURI(
+       'vap+cdaweb:ds=OMNI2_H0_MRG1HR&id=DST1800&timerange=Oct+2016')
 apds.doGetDataSet()
