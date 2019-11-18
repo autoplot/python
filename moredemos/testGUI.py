ebd48cf2dbe622f092d6ea66a6766d7c1e42b8bf
Jeremy Faden <faden@cottagesystems.com>
2019-11-18 11:29:09 -0600

    Create testGUI.py

---
+++ moredemos/testGUI.py
@@ -0,0 +1,10 @@
+from autoplot import *
+import jpype
+
+print('This will cause some platforms to hang!')
+
+org = addjavapath('http://autoplot.org/latest/autoplot.jar')
+javax= jpype.JPackage('javax')
+javax.swing.JOptionPane.showMessageDialog(None, 'Java Swing is Okay')
+
+print('Things didn't hang, so this platform is okay.')
