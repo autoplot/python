7367b1e02a2f4ce4880229170254d491a4bf8c53
jbfaden <faden@cottagesystems.com>
2019-11-18 10:48:42 -0600

    demo of to_qdataset code

--- moredemos/to_qdataset_demo.py
+++ moredemos/to_qdataset_demo.py
@@ -1,22 +1,24 @@
-# This does not work presently.
-
+import numpy as np
+import matplotlib.mlab as mlab
 from autoplot import *
+
+
 org = javaaddpath('http://autoplot.org/jnlp/latest/autoplot.jar')
 
 org.autoplot.ScriptContext.createGui()
 
-zz= to_qdataset( [ 1,2,3,4,5,6,4,3,2,1 ] )
+zz = to_qdataset([1, 2, 3, 4, 5, 6, 4, 3, 2, 1])
+applot(zz)
 
-applot( zz )
-
-import numpy as np
 delta = 0.025
 x = np.arange(-3.0, 3.0, delta)
 y = np.arange(-2.0, 2.0, delta)
 X, Y = np.meshgrid(x, y)
-import matplotlib.mlab as mlab
 Z1 = mlab.bivariate_normal(X, Y, 1.0, 1.0, 0.0, 0.0)
 Z2 = mlab.bivariate_normal(X, Y, 1.5, 0.5, 1, 1)
 Z = 10.0 * (Z2 - Z1)
-ds= to_qdataset( x, y, Z )
-applot( ds )
+
+org = javaaddpath('http://autoplot.org/jnlp/latest/autoplot.jar')
+org.autoplot.ScriptContext.createGui()
+applot(x, y, Z)  # will convert to QDataSet internally
+
