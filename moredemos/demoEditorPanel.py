75e9897e553a8a4f6f773ab0c042b3d7a29af316
Jeremy Faden <faden@cottagesystems.com>
2019-11-19 06:34:51 -0600

    Update demoEditorPanel.py

--- moredemos/demoEditorPanel.py
+++ moredemos/demoEditorPanel.py
@@ -1,10 +1,17 @@
+'''This uses Autoplot Java/Swing GUIs to create URIs'''
 from autoplot import *
 import jpype
 
-org= javaaddpath('http://autoplot.org/latest/autoplot.jar')
-javax= jpype.JPackage('javax')
+org = javaaddpath('http://autoplot.org/latest/autoplot.jar')
+
+javax = jpype.JPackage('javax')
+java = jpype.JPackage('java')
+WindowManager = org.autoplot.datasource.WindowManager
 
 oldURI = 'vap+cdaweb:'
+
+parent = javax.swing.JPanel()
+parent.setLayout( java.awt.BorderLayout() )
 
 DataSourceEditorPanelUtil = org.autoplot.datasource.DataSourceEditorPanelUtil
 p = DataSourceEditorPanelUtil.getDataSourceEditorPanel(None,oldURI);
