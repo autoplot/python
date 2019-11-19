2b596a04fca0613df99eed3ec2518711bb4cf997
Jeremy Faden <faden@cottagesystems.com>
2019-11-19 06:37:30 -0600

    Update demoEditorPanel.py

--- moredemos/demoEditorPanel.py
+++ moredemos/demoEditorPanel.py
@@ -4,18 +4,17 @@ import jpype
 
 org = javaaddpath('http://autoplot.org/latest/autoplot.jar')
 
-javax = jpype.JPackage('javax')
-java = jpype.JPackage('java')
-WindowManager = org.autoplot.datasource.WindowManager
-
 oldURI = 'vap+cdaweb:'
 
+javax = jpype.JPackage('javax')
+java = jpype.JPackage('java')
 parent = javax.swing.JPanel()
 parent.setLayout( java.awt.BorderLayout() )
 
 DataSourceEditorPanelUtil = org.autoplot.datasource.DataSourceEditorPanelUtil
-p = DataSourceEditorPanelUtil.getDataSourceEditorPanel(None,oldURI);
+p = DataSourceEditorPanelUtil.getDataSourceEditorPanel(parent,oldURI);
 
+WindowManager = org.autoplot.datasource.WindowManager
 if WindowManager.OK_OPTION == WindowManager.showConfirmDialog(
     None, p, "Editing URI", WindowManager.OK_CANCEL_OPTION ):
                                                           
