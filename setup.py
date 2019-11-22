6a6236b987f48cf7dc03a4eb39add1c18cdc5e0f
jbfaden <faden@cottagesystems.com>
2019-11-22 10:04:20 -0600

    new version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if sys.argv[1] == 'develop':
 
 setup(
     name='autoplot',
-    version='0.3.5',
+    version='0.3.6',
     author='Jeremy Faden',
     author_email='faden@cottagesystems.com',
     packages=find_packages(), 
