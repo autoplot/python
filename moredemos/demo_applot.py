62dd239b5aeb1d83926cb79c047b93f3d0cd91f6
Jeremy Faden <faden@cottagesystems.com>
2023-04-12 12:33:32 -0500

    Update demo_applot.py

--- moredemos/demo_applot.py
+++ moredemos/demo_applot.py
@@ -1,6 +1,7 @@
 import autoplot as ap
+import jpype
 org = ap.javaaddpath()
-apds = org.autoplot.idlsupport.APDataSet()
+apds = jpype.JClass('org.autoplot.idlsupport.APDataSet')()  # instaciate the class
 apds.loadDataSet( 'vap+cdaweb:ds=OMNI2_H0_MRG1HR&id=DST1800&timerange=Oct+2016' )
 epoch = ap.to_ndarray( apds,'Epoch' )
 dst = ap.to_ndarray( apds,'DST' )
