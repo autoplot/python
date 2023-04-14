a00d3752d5dc1c479705623cff0314b7babc3ab9
Jeremy Faden <faden@cottagesystems.com>
2023-04-14 15:03:12 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,8 @@
 from autoplot import javaaddpath,toDateTime
+import autoplot as ap
 
-org = javaaddpath('http://autoplot.org/jnlp/latest/autoplot.jar')
-apds = org.autoplot.idlsupport.APDataSet()
+javaaddpath('http://autoplot.org/jnlp/latest/autoplot.jar')
+apds = ap.APDataSet()
 
 apds.setDataSetURI('vap+cdaweb:ds=OMNI2_H0_MRG1HR&id=DST1800&timerange=Oct+2016')
 apds.doGetDataSet()
