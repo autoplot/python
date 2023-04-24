10b69a8ff63832d61bd659e453ada79abee1d6dc
Jeremy Faden <faden@cottagesystems.com>
2023-04-24 10:43:57 -0500

    update to current version

--- demo.py
+++ demo.py
@@ -1,16 +1,15 @@
-from autoplot import javaaddpath,toDateTime
 import autoplot as ap
 
-javaaddpath('http://autoplot.org/jnlp/latest/autoplot.jar')
+ap.javaaddpath('http://autoplot.org/jnlp/latest/autoplot.jar')
 apds = ap.APDataSet()
 
 apds.setDataSetURI('vap+cdaweb:ds=OMNI2_H0_MRG1HR&id=DST1800&timerange=Oct+2016')
 apds.doGetDataSet()
 
-print( apds.toString() )
+print( apds.toString() ) # Shows DST with timetags "Epoch" is loaded.
 
-vv= apds.values('DST' )
-tt= toDateTime( apds, 'Epoch' )
+vv= ap.to_ndarray(apds)
+tt= ap.to_ndarray(apds, 'Epoch')
 
 from matplotlib import pyplot as plt
 
